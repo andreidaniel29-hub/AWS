f034c1206e6db2a693fdd1809aff53b85793c229
daniel andrei <andrei.cogian@educa.madrid.org>
2025-11-05 13:45:37 -0500

    Añadiendo nuevos ficheros

---
+++ Proyecto-login/procesar_registro.php
@@ -0,0 +1,12 @@
+<?php
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+$file = fopen("usuarios.txt","a");
+
+fwrite($file, $usuario . ":" . password_hash($password, PASSWORD_DEFAULT) . "\n");
+fclose($file);
+
+
+echo "<h1>Usuario registrado correctamente</h1>"; 
+echo "<p><a href='login.php'>Iniciar sesión</a></p>"; 
---
+++ who/index.php
@@ -0,0 +1,4 @@
+<?php
+    echo "<h1>¡Hola,mundo desde php en apache!</h1>";
+
+?>
